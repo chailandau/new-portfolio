f90dbc1652ea05fa6b5c9e2f11dc3ba9cdf49cdd
Chai Landau <clandau@cliquestudios.com>
2022-02-03 17:47:42 -0500

    init setup

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header>
+      <div className="wrapper">
+        <h1>Chai Landau</h1>
+      </div>
+    </header>
+  );
+};
+  
+export default Header;
+  
---
+++ components/Layout.js
@@ -0,0 +1,22 @@
+import Head from "next/head";
+import Header from "./Header";
+
+const Layout = ( { children } ) => {
+  return (
+    <>
+      <Head>
+        <title>Chai Landau</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <main>
+        {children}
+      </main>
+      <footer>
+         
+      </footer>
+    </>
+  );
+};
+  
+export default Layout;
